4bc73f5571b206ca165742af44df36b1853bb93f
EIndriksons <edgars@indriksons.lv>
2020-04-29 21:10:02 +0300

    Project 0 - implemented newTodo()

--- project0/script.js
+++ project0/script.js
@@ -1,14 +1,33 @@
 const classNames = {
-  TODO_ITEM: 'todo-container',
-  TODO_CHECKBOX: 'todo-checkbox',
-  TODO_TEXT: 'todo-text',
-  TODO_DELETE: 'todo-delete',
+    TODO_ITEM: 'todo-container',
+    TODO_CHECKBOX: 'todo-checkbox',
+    TODO_TEXT: 'todo-text',
+    TODO_DELETE: 'todo-delete',
 }
 
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+itemCount = 0
+uncheckedCount = 0
+
+/* adds new ToDo entry */
 function newTodo() {
-  alert('New TODO button clicked!')
+    const ListItem = `
+        <li class="` + classNames.TODO_ITEM + `">
+            <input class="` + classNames.TODO_CHECKBOX + `" type="checkbox">
+            <input class="` + classNames.TODO_TEXT + `" type="text">
+            <button class="` + classNames.TODO_DELETE + `" type="button">Delete</button>
+        </li>
+    `
+    $(list).append(ListItem)
+
+    // increment item count
+    itemCount += 1
+    $(itemCountSpan).text(itemCount)
+
+    // increment unchecked count
+    uncheckedCount += 1
+    $(uncheckedCountSpan).text(uncheckedCount)
 }
