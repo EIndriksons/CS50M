9fcc3c2757eb390fd7a15ebc0725b3850271eead
EIndriksons <edgars@indriksons.lv>
2020-05-11 18:21:54 +0300

    Project 0 - Final version

--- lecture1/project0-react/todoApp.js
+++ lecture1/project0-react/todoApp.js
@@ -5,8 +5,8 @@ let id = 0
 
 const Todo = props => (
     <li>
-        <input type="checkbox"/>
-        <button>Delete</button>
+        <input type="checkbox" checked={props.todo.checked} onChange={props.onToggle} />
+        <button onClick={props.onDelete}>Delete</button>
         <span>{props.todo.text}</span>
     </li>
 )
@@ -23,16 +23,46 @@ class App extends React.Component {
     addTodo() {
         const text = prompt("Todo text please!")
         this.setState({
-            todos: [...this.state.todos, {id: id++, text: text}]
+            todos: [
+                ...this.state.todos, 
+                {id: id++, text: text, checked: false}
+            ]
+        })
+    }
+
+    removeTodo(id) {
+        this.setState({
+            todos: this.state.todos.filter(todo => todo.id !== id)
+        })
+    }
+
+    toggleTodo(id) {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id !== id) return todo
+                return {
+                    id: todo.id,
+                    text: todo.text,
+                    checked: !todo.checked
+                }
+            })
         })
     }
 
     render() {
         return (
             <div>
+                <div>Todo count: {this.state.todos.length}</div>
+                <div>Unchecked Todo count: {this.state.todos.filter(todo => !todo.checked).length}</div>
                 <button onClick={() => this.addTodo()}>Add Todo</button>
                 <ul>
-                    {this.state.todos.map(todo => <Todo todo={todo} />)}
+                    {this.state.todos.map(todo =>
+                        <Todo 
+                        onToggle={() => this.toggleTodo(todo.id)}
+                        onDelete={() => this.removeTodo(todo.id)}
+                        todo={todo}
+                        />
+                    )}
                 </ul>
             </div>
         )
